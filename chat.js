13edc1538a70f3398bdc75b8f0d70587da6f84f1
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-05-09 13:42:07 -0400

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,4 +1,4 @@
 function findName(){
     UserName=localStorage.getItem("UserName");
-    document.getElementById("404").innerHTML="Hello ,"+UserName+".Thanks for coming ,but their is no page here"
+    document.getElementById("welcome").innerHTML="Hello ,"+UserName;
 }
--- login.js
+++ login.js
@@ -1,3 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCmE3yIrSMKYDadEkgPKlrUG4AdtaJS_nI",
+    authDomain: "letschat-1fc01.firebaseapp.com",
+    databaseURL: "https://letschat-1fc01-default-rtdb.firebaseio.com",
+    projectId: "letschat-1fc01",
+    storageBucket: "letschat-1fc01.appspot.com",
+    messagingSenderId: "1083839247742",
+    appId: "1:1083839247742:web:d3cb3b7c495984471aef2f"
+  };
+  firebase.initializeApp(firebaseConfig);
 function login(){
     username=document.getElementById("UserName").value;
     localStorage.setItem("UserName",username);
