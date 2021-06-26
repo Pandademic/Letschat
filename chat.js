623df52a65d21e1b020aae80db53677543159b12
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-06-26 13:27:40 -0400

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,4 +1,24 @@
-function findName(){
-    UserName=localStorage.getItem("UserName");
-    document.getElementById("404").innerHTML="Hello ,"+UserName+".Thanks for coming ,but their is no page here"
+/*var firebaseConfig = {
+    apiKey: "AIzaSyCmE3yIrSMKYDadEkgPKlrUG4AdtaJS_nI",
+    authDomain: "letschat-1fc01.firebaseapp.com",
+    databaseURL: "https://letschat-1fc01-default-rtdb.firebaseio.com",
+    projectId: "letschat-1fc01",
+    storageBucket: "letschat-1fc01.appspot.com",
+    messagingSenderId: "1083839247742",
+    appId: "1:1083839247742:web:d3cb3b7c495984471aef2f"
+  };
+  firebase.initializeApp(firebaseConfig);
+  
+function logout(){
+    window.location="login.html"
+}
+function setItems(){
+    console.log("started.....")
+    roomName=localStorage.getItem("roomName=");
+    console.log(roomname);
+    
+}
+*/
+function logout(){
+    window.location="login.html";
 }
---
+++ page.js
@@ -0,0 +1,63 @@
+//Vars
+var firebaseConfig = {
+   apiKey: "AIzaSyAx6OkMhGcuTwSA8lyP2xXIkSFNxrQ7N_Y",
+   authDomain: "letschat-a8134.firebaseapp.com",
+   databaseURL: "https://letschat-a8134-default-rtdb.firebaseio.com",
+   projectId: "letschat-a8134",
+   storageBucket: "letschat-a8134.appspot.com",
+   messagingSenderId: "149667303219",
+   appId: "1:149667303219:web:6ff00f98afbe65b4067846"
+ };
+var user_name=localStorage.getItem("UserName");
+var room_name;
+//Test Case
+console.log("Firebase Config:"+firebaseConfig);
+console.log("UserName:"+user_name);
+//console.log("Roomname:"+room_name);
+//Activate Firebase
+firebase.initializeApp(firebaseConfig);
+//Func
+function RoomDisplay(){
+room_name=document.getElementById("roomname").value;
+//test case
+console.log("Room Name:"+room_name);
+}
+function redirectToRoomName(){
+   console.log(room_name);
+   localStorage.setItem("room_name",room_name);
+   firebase.database().ref("/").child(room_name).update({
+      purpose:"adding room name"
+});
+   window.location="chatPage.html";
+}
+/*function addRoom(){
+   var room_name=document.getElementById("roomname").value;
+   firebase.database().ref("/").child(room_name).update({
+         purpose:"adding room name"
+   });
+   localStorage.setItem("room_name",room_name);
+   window.location="chatPage.html";
+}
+*/
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {
+   document.getElementById("list").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+var Room_names = childKey;
+//Start code
+console.log("room name-"+Room_names);
+var row="<div class='room_name'id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+document.getElementById("list").innerHTML+=row;
+
+//End code
+});});}
+/*getData();
+function redirectToRoomName(name){
+console.log(name);
+localStorage.setItem("room_name",name);
+window.localStorage="kwitter_page.html";
+}
+function logOut(){
+localStorage.removeItem("UserName");
+localStorage.removeItem("room_name");
+window.location="login.html";
+}
+*/
